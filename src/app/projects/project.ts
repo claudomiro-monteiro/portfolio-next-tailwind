fb6cb28d05fc0fdf003c23846f0226a7c6f6fbaa
claudomiro <claudomiromonteiro@gmail.com>
2024-02-13 16:38:43 -0300

    :rocket: add projects in projects

--- src/app/projects/project.ts
+++ src/app/projects/project.ts
@@ -2,10 +2,28 @@ import ignews from '../../../public/repo/ignews.png'
 import expenses from '../../../public/repo/expenses.png'
 import ecoleta from '../../../public/repo/ecoleta.png'
 import coffee from '../../../public/repo/coffee.png'
+import expert from '../../../public/repo/expert.png'
+import devstore from '../../../public/repo/devstore.png'
 
 export const projects = [
   {
     id: 1,
+    title: 'NLW-Expert',
+    img: expert,
+    description:
+      'Projeto desenvolvido com Vite, Tailwindcss, sonner, radix entre outras funcionalidades.',
+    rote: 'https://github.com/claudomiro-monteiro/nlw-expert-react',
+  },
+  {
+    id: 2,
+    title: 'DevStore',
+    img: devstore,
+    description:
+      'Projeto desenvolvido com Next.js, Typescript, Tailwindcss, Cypress, Zod, entre outras funcionalidades.',
+    rote: 'https://github.com/claudomiro-monteiro/devstore',
+  },
+  {
+    id: 2,
     title: 'IgNews',
     img: ignews,
     description:
@@ -13,7 +31,7 @@ export const projects = [
     rote: 'https://github.com/claudomiro-monteiro/ignews',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Expenses',
     img: expenses,
     description:
@@ -21,7 +39,7 @@ export const projects = [
     rote: 'https://github.com/claudomiro-monteiro/expenses-front-end',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Ecoleta',
     img: ecoleta,
     description:
@@ -29,18 +47,11 @@ export const projects = [
     rote: 'https://github.com/claudomiro-monteiro/ecoleta',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Great Coffee',
     img: coffee,
     description:
       'Projeto desenvolvido com ReactJs, CSS, Este foi um desafio da Rocketseat.',
     rote: 'https://github.com/claudomiro-monteiro/RocketCoffee',
   },
-  // {
-  //   id: 5,
-  //   title: 'Ignews',
-  //   img: ignews,
-  //   description:
-  //     'Projeto desenvolvido com NextJs, Sass, integração com a API do Stripe.',
-  // },
 ]
